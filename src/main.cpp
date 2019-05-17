a5eab3f996a126b863a45b79188a2635bcfccc58
Neel Kowdley <nkowdley@gmail.com>
2019-05-17 14:27:37 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -153,7 +153,10 @@ int main() {
           tk::spline s;
 
           s.set_points(ptsx, ptsy);
-
+          
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          
           for (int i = 0; i<previous_path_x.size(); i++)
           {
             next_x_vals.push_back(previous_path_x[i]);
@@ -163,7 +166,30 @@ int main() {
           double target_x = 30.0;
           double target_y = s(target_x);
           double target_dist = sqrt((target_x)*(target_x)+(target_y)*target_y));
-
+          
+          double x_add_on = 0;
+          
+          for (int i = 1; i <= 50-previous_path_x.size();i++)
+          {
+            double N = (target_dist/(.02*ref_vel/2.24));
+            double x_point = x_add_on + (target_x)/N;
+            double y_point = s(x_point);
+            
+            x_add_on = x_point;
+            
+            double x_ref = x_point;
+            double y_ref = y_point;
+            
+            x_point = (x_ref * cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw)+y_ref*cos(ref_yaw));
+            
+            x_point += ref_x;
+            y_point += ref_y;
+            
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+            
+          }
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
